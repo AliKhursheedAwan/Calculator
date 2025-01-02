15401aa4a12b7ed32778706aa44ec2444048f9a2
Ibraheem Khursheed <ibraheemkhursheed20@gmail.com>
2025-01-03 00:00:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let inputBox = document.getElementById("inputBox");
+
+let buttons = document.querySelectorAll("button");
+
+let string = ""
+
+
+buttons.forEach(element =>{
+    element.addEventListener("click", (b)=>{
+        if (b.target.innerText === "=") {
+            try {
+                string = eval(string).toString();
+                inputBox.value = string;
+            } catch (e) {
+                inputBox.value = "Error";
+                string = "";
+            }
+        }
+        else if(b.target.innerText == "AC"){
+            string = "";
+            inputBox.value = string;
+        }
+        else if(b.target.innerText == "DEL"){
+            string = string.substring(0, string.length-1);
+            inputBox.value = string;
+        }
+        else if(b.target.id == "plusMinus"){
+            string = string(-eval(string));
+            inputBox.value = string;
+
+        }
+    
+        else{
+            string += b.target.innerText;
+            inputBox.value = string;
+        }
+    })
+})
